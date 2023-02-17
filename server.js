3e86f45dd47545068bea1eceaed8d26ede2bd5fc
Naveenchandar <naveenchandece@gmail.com>
2023-02-17 13:45:33 +0530

    cors module and config options

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+const whiteList = ['http://localhost:3500', 'http://localhost:3000', 'https://www.google.com']
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 console.log('HELLO WORLD');
 
